b8c1ed46f7dc24d28e22642e0b080081bf3e0e74
Razgrizsx <razgrisss41@gmail.com>
2023-08-25 21:14:38 -0300

    First Commit

---
+++ src/app/shared/menu/menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent {
+  items : MenuItem[] = []
+}
